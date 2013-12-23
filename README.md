644a14f9201821bb832a3727216eb926ad80c118
Daniel Thies <daniel@thies-desktop.ccal.edu>
2013-12-23 12:31:10 -0600

    initial version local plugin

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-local_mathslate
-===============
+atto_mathslate
+==============
 
-Common files for Mathslate Plugins
+Graphical Equation Editor Moodle plugin for Atto editor
+
+This is a prototype. Do not use in production enviroment. The functionality will 
+change in future versions, and bugs are expected.
+
+Install in Moodle directory lib/editor/atto/plugins/mathslate
+
+This plugin assumes MathJax is already included and configured in the Moodle installation. 
---
+++ lib.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto text editor integration version file.
+ *
+ * @package    atto_mathslate
+ * @copyright  2013 Daniel Thies <dthies@ccal.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Initialise this plugin
+ * @param string $elementid
+ */
+function local_mathslate_init($elementid) {
+    global $PAGE, $OUTPUT, $CFG;
+
+    $PAGE->requires->strings_for_js(array( 'mathslate'),'local_mathslate');
+
+    $PAGE->requires->yui_module('moodle-local_mathslate-editor');
+
+}
